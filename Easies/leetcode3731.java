2e5d25f6d8df93abca18a614982bb571e1ec7ddb
prefetch4do! <130607739+OZALOMARLI@users.noreply.github.com>
2025-11-15 23:03:07 +0300

    Add files via upload

---
+++ Easies/leetcode3731.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class Solution{
+    public List<Integer> findMissingElements(int[] nums) {
+
+        int minVal = Integer.MAX_VALUE;
+        int maxVal = Integer.MIN_VALUE;
+
+        Set<Integer> presentElements = new HashSet<>(); 
+
+        List<Integer> missingElements = new ArrayList<>();
+
+        for (int num : nums) {
+            minVal = Math.min(minVal, num); 
+
+
+            maxVal = Math.max(maxVal, num); 
+
+
+            presentElements.add(num); 
+        }
+
+
+        for (int i = minVal + 1; i < maxVal; i++) {
+
+            if (!presentElements.contains(i)) {
+                missingElements.add(i); 
+            }
+        }
+        return missingElements;
+    }
+}
